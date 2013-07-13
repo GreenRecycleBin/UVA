f6037c1eaa1480959200e75bf8d7f95818758988
Daniel Le <GreenRecycleBin@gmail.com>
2013-07-13 11:48:06 +0800

    Use easier to understand algorithm for LIS

--- Volume-001/prob111.cpp
+++ Volume-001/prob111.cpp
@@ -3,70 +3,63 @@
 
 using namespace std;
 
-int LongestIncreasingSubsequenceLength(const vector<int>&);
-int MaximumIndexOfTailLessThan(const vector<int>&, int, int);
+int CalculateLengthOfLongestIncreasingSubsequence(const vector<int>&);
+int IndexOfLeftMostElementGreaterThan(int, const vector<int>&);
 
-const int MAX_N = 20;
-
-int main()
-{
+int main() {
   int n;
   scanf("%d", &n);
-  
-  vector<int> events_to_indices(MAX_N);
-  
+
+  vector<int> events_to_indices(n);
+
   for (int i = 0, rank; i < n; ++i)
     scanf("%d", &events_to_indices[i]);
-  
+
   // Current indices to original indices
-  vector<int> indices_to_indices(MAX_N);
-  
+  vector<int> indices_to_indices(n);
+
   for (int rank; scanf("%d", &rank) == 1;) {
     indices_to_indices[rank - 1] = events_to_indices[0];
-    
+
     for (int i = 1; i < n; ++i) {
       scanf("%d", &rank);
       indices_to_indices[rank - 1] = events_to_indices[i];
     }
-    
-    printf("%d\n", LongestIncreasingSubsequenceLength(indices_to_indices));
+
+    printf("%d\n",
+           CalculateLengthOfLongestIncreasingSubsequence(indices_to_indices));
   }
-  
+
   return 0;
 }
 
-int LongestIncreasingSubsequenceLength(const vector<int> &v)
-{
-  int size = v.size(), length;
-  vector<int> tails(size);
-  tails[0] = v[0];
-  length = 1;
-  
-  for (int i = 1; i < size; ++i) {      
-    // New minimum value
-    if (v[i] < tails[0])
-      tails[0] = v[i];
-    else if (v[i] > tails[length - 1])
-    // Extend the largest subsequence
-      tails[length++] = v[i];
-    else
-      tails[MaximumIndexOfTailLessThan(tails, length, v[i])] = v[i];
+int CalculateLengthOfLongestIncreasingSubsequence(const vector<int> &v) {
+  if (v.empty()) return 0;
+
+  vector<int> t;
+  t.push_back(v.front());
+
+  for (int i = 1; i < v.size(); ++i) {
+    int last = t.back();
+    int current = v[i];
+
+    if (current > last) t.push_back(current);
+    else t[IndexOfLeftMostElementGreaterThan(current, t)] = current;
   }
-  
-  return length;
+
+  return t.size();
 }
 
-int MaximumIndexOfTailLessThan(const vector<int> &v, int size, int key)
-{
-  // Check the last element first when the vector have only two elements
-  int left = -1, right = size - 1, middle;
-  
-  // Return when the list have only one element
-  while (left + 1 < right) {
-    middle = left + (right - left) / 2;
-    
-    (v[middle] >= key ? right : left) = middle;
+int IndexOfLeftMostElementGreaterThan(int key, const vector<int> &v) {
+  int left = 0, right = v.size();
+
+  while (left != right) {
+    int middle = left + (right - left) / 2;
+    int current = v[middle];
+
+    if (current > key) right = middle;
+    else left = middle + 1;
   }
-  
+
   return right;
 }
